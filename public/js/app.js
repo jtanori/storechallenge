6e5a7f69412e36ea672bada7b87364eb7bd35b1a
nestorin <nestor.vda@gmail.com>
2016-06-22 02:02:04 -0700

    Main styles and product display

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 angular
     .module('app', ['ui.router'])
+    .constant('API_URL', 'http://localhost:5000/api')
     .config(function($stateProvider, $urlRouterProvider) {
         //Default
         $urlRouterProvider.otherwise("index");
@@ -16,13 +17,28 @@ angular
                     },
                     "menu": {
                         templateUrl: "templates/menu.html",
-                        controller: 'ProductsCtrl'
+                        controller: 'ProductsCtrl',
+                        resolve: {
+                            products: function(ProductService, $rootScope) {
+                                $rootScope.loading = true;
+
+                                return ProductService
+                                    .fetch()
+                                    .then(function(items){
+                                        return items;
+                                    }, function(e){
+                                        return e;
+                                    })
+                                    .finally(function(){
+                                        $rootScope.loading = false;
+                                    });
+                            }
+                        }
                     }
                 }
             });
     })
-    .run(function($rootScope){
-        console.log('run');
+    .run(function($rootScope, $window){
         $rootScope.$on('$stateChangeStart', function (event, next, nextParams, fromState) {
             console.log('change start', arguments);
         });
@@ -30,10 +46,41 @@ angular
         $rootScope.$on('$stateNotFound', function(event, unfoundState, fromState, fromParams){
             console.log('state not found', arguments);
         });
+
+        // I could have used an abstract state in the router but for brevity
+        // let's set this unique function here
+        $rootScope.up = function(){
+            $window.scrollTo(0,0);
+        }
     })
-    .controller('ProductsCtrl', function(){
-        console.log('products controller');
+    .controller('ProductsCtrl', function($scope, $timeout, products){
+        $timeout(function(){
+            $scope.$apply(function(){
+                $scope.items = products;
+            });
+        });
+
+        $scope.add = function(product){
+            console.log('add', product);
+        }
     })
     .controller('CartCtrl', function(){
         console.log('main controller');
     })
+    .factory('ProductService', function($http, $q, API_URL){
+        return {
+            fetch: function(){
+                var q = $q.defer();
+
+                $http
+                    .get(API_URL + '/products')
+                    .then(function(response){
+                        q.resolve(response.data);
+                    }, function(e){
+                        q.reject(e);
+                    });
+
+                return q.promise;
+            }
+        }
+    });
